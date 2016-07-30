1085241c3f781f5b0aab30b30fd52ba82f950dd1
Kyoung-chan SOHN <xinuguru@sk.com>
2016-07-30 13:07:58 +0900

    more readable code

--- monitor-appstore.py
+++ monitor-appstore.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env python
+# coding: utf-8
 
-import urllib2, re, json, webbrowser, random, time
+import urllib2
+import re
+import json
+import webbrowser
+import random
+import time
 
 def isFree(url):
-    SERVER_DATA = "its.serverData="
     time.sleep(random.random() * 100)
     html = urllib2.urlopen(url).read()
-    jsobjects = re.findall("<script.*>(.*?)</script>", html)
+    jsobjects = re.findall("<script.*>its.serverData=(.*)</script>", html)
 
-    for jsobj in jsobjects:
-        if jsobj.startswith(SERVER_DATA):
-            serverData = json.loads(jsobj[len(SERVER_DATA):])
-            return serverData["pageData"]["isFree"]
+    if jsobjects:
+        print url, jsobjects[0]
+        serverData = json.loads(jsobjects[0])
+        return serverData["pageData"]["isFree"]
+
+    return False
 
 urls = [
-    #"https://itunes.apple.com/kr/app/instaplace/id565105760?mt=8",
     "https://itunes.apple.com/kr/app/avplayer-mu-inkoding-sogdojojeol/id395680819?mt=8",
-    "https://itunes.apple.com/kr/app/teoboseukaen-yeongsujeung/id342548956?mt=8",
-    #"https://itunes.apple.com/kr/app/gwan-utd/id599900813?mt=8",
 ]
 
 
 random.seed()
-for url, free in zip(urls, map(isFree, urls)):
-    if free:
+for url in urls:
+    if isFree(url):
         webbrowser.open(url, new=2, autoraise=True)
